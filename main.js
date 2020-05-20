20d5f171a0cb5104dcfc2c35b696592b4f11a574
nikitagru <50076302+nikitagru@users.noreply.github.com>
2020-05-20 14:08:46 +0500

    fixed reverse RPN and added exceptions

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 let operations = new Map(); // collection of operations
 
 operations.set("(", 0);  //key - type of the operation 
@@ -12,7 +14,7 @@ let operationStack = [];
 let resultStack = [];
 
 
-let _expressionStr = "ab+c*";   //"(a+b)*c";   //process.argv[1];  // user string from cmd
+let _expressionStr = "ab+hpl*+*cd/*f+";//"(a+b)*(h+p*l)*(c/d)+f";   //"(a+b)*c";   //process.argv[1];  // user string from cmd
 //_expressionStr = _expressionStr.toString();
 let bracketCount = 0;
 let operandCount = 0;
@@ -20,107 +22,161 @@ let operationsCount = 0;
 
 let isKyr = /[а-яё]/i.test(_expressionStr);
 let isLat = /[a-z]/i.test(_expressionStr);
-
-
+let invSym = false;
 for (let i = 0; i < _expressionStr.length; i++) {
-    if (_expressionStr[i] === "(" || _expressionStr[i] === ")") {
-        bracketCount++;
-    }
-    if (isLat) {
-        if (!(operations.has(_expressionStr[i]))) {
-            operandCount++;
-        }
-        
-    }
-    if (operations.has(_expressionStr[i])) {
-        if (_expressionStr[i] !== "(" && _expressionStr[i] !== ")") {
-            operationsCount++;
+    if (!(operations.has(_expressionStr[i]))) {
+        if (/[^\d\sA-Z]/gi.test(_expressionStr[i])) {
+            invSym = true;
         }
     }
 }
-
-if (isKyr) {
-    console.log("Exception: invalid symbol(-s)");
-} else if (operandCount > operationsCount + 1) {
-    console.log("Exception: operation pass");
-} else if (operationsCount >= operandCount) {
-    console.log("Exception: heaps of expression");
-} else if (bracketCount % 2 != 0) {
-    console.log("Exception: bracket pass");
-} else {
-    let toRPN = true;
-    for (let k = 1; k < _expressionStr.length; k++) {
-        if (!(operations.has(_expressionStr[k-1])) && !(operations.has(_expressionStr[k]))) {
-            toRPN = false;
-        }
-    }
-    if (toRPN) {
-        for (let i = 0; i < _expressionStr.length; i++) {
     
+
+
+let openBracket = false;
+let closeBracket = false;
+
+let toRPN = true;
+
+
+
+for (let k = 1; k < _expressionStr.length; k++) {
+    if (!(operations.has(_expressionStr[k-1])) && !(operations.has(_expressionStr[k]))) {
+        toRPN = false;
+    }
+}
+if (toRPN) {
+    for (let i = 0; i < _expressionStr.length; i++) {
+        if (_expressionStr[i] === "(" || _expressionStr[i] === ")") { 
+            bracketCount++;
+        }
+        if (isLat) {
             if (!(operations.has(_expressionStr[i]))) {
-                resultStack.push(_expressionStr[i]);        //if symbol with index "i" is operand - push this into resultStack
-            } else if (operations.has(_expressionStr[i])) {
-                if (operationStack.length == 0) {
-                    operationStack.push(_expressionStr[i]);     //if operationStack is empty - push operator into operationStack
-                } else if(_expressionStr[i] === ")"){        
-                    let bracketIndex = operationStack.indexOf("(");     //index of first "("
-                    let temp = operationStack.length - 1;       //last index of the operationStack
-        
-                    for(let i = temp; i > bracketIndex; i--) {
-                        resultStack.push(operationStack.pop());        //delete and push into result from last to first "("
-                    }
-                    operationStack.pop();
-        
-                } else {
-                    let currentPririty = operations.get(_expressionStr[i]);        //pririty of the operator with index "i"
-        
-                    let lastIndex = operationStack.length - 1;
-                    let prevOperator = operationStack[lastIndex];       //previous operator in the stack with operations
-                    let prevPririty = operations.get(prevOperator);        //priority of the previous operator
-                    
-                    if (currentPririty <= prevPririty && _expressionStr[i] != "(") {        //if priority of the operation with index "i" is greater than priority of the prev operator
-                        let j = operationStack.length;
-                        while(j!=0) {
-                            resultStack.push(operationStack.pop());     //delete operation from operationStack and push into resultStack
-                            if (prevPririty < currentPririty) {
-                                break;
-                            }
-                            j--;
-                        }
-                        
-        
-                        operationStack.push(_expressionStr[i]);     //push operation with index "i"
-                    } else {
-                        operationStack.push(_expressionStr[i]);
-                    }
-        
+                operandCount++;
+                if (operandCount == 2) {
+                    throw "Exception: operation pass";
                 }
             }
-        
         }
+        if (operations.has(_expressionStr[i])) {
+            if (_expressionStr[i] !== "(" && _expressionStr[i] !== ")") {
+                operationsCount++;
+                operandCount = 0;
+            }
+        }
+        if (_expressionStr[i] === "(") {
+            openBracket = true;
+        } else if (_expressionStr[i] === ")") {
+            if (!openBracket) {
+                throw "Exception: invalid brackets direction";
+            } else {
+                closeBracket = true;
+                openBracket = false;
+            }
+        }
+        if (isKyr || invSym) {
+            throw "Exception: invalid symbol(-s)"; 
+        }
+        
+        
+
+        if (!(operations.has(_expressionStr[i]))) {
+            resultStack.push(_expressionStr[i]);        //if symbol with index "i" is operand - push this into resultStack
+        } else if (operations.has(_expressionStr[i])) {
+            if (operationStack.length == 0) {
+                operationStack.push(_expressionStr[i]);     //if operationStack is empty - push operator into operationStack
+            } else if(_expressionStr[i] === ")"){        
+                let bracketIndex = operationStack.indexOf("(");     //index of first "("
+                let temp = operationStack.length - 1;       //last index of the operationStack
+    
+                for(let i = temp; i > bracketIndex; i--) {
+                    resultStack.push(operationStack.pop());        //delete and push into result from last to first "("
+                }
+                operationStack.pop();
+    
+            } else {
+                let currentPririty = operations.get(_expressionStr[i]);        //pririty of the operator with index "i"
+    
+                let lastIndex = operationStack.length - 1;
+                let prevOperator = operationStack[lastIndex];       //previous operator in the stack with operations
+                let prevPririty = operations.get(prevOperator);        //priority of the previous operator
+                
+                if (currentPririty <= prevPririty && _expressionStr[i] != "(") {        //if priority of the operation with index "i" is greater than priority of the prev operator
+                    let j = operationStack.length;
+                    while(j!=0) {
+                        resultStack.push(operationStack.pop());     //delete operation from operationStack and push into resultStack
+                        if (prevPririty < currentPririty) {
+                            break;
+                        }
+                        j--;
+                    }
+                    operationStack.push(_expressionStr[i]);     //push operation with index "i"
+                } else {
+                    operationStack.push(_expressionStr[i]);
+                }
+    
+            }
+        }
+    
+    }
     } else {
-        for (let i = 0; i <_expressionStr.length; i++) {
+        let counterStep = _expressionStr.length;
+        resultStack.push("(");
+        for (let i = 0; i < _expressionStr.length; i++) {
+            if (_expressionStr[i] === "(" || _expressionStr[i] === ")") { 
+                bracketCount++;
+            }
+            if (isLat) {
+                if (!(operations.has(_expressionStr[i]))) {
+                    operandCount++;
+                }
+            }
+            if (operations.has(_expressionStr[i])) {
+                if (_expressionStr[i] !== "(" && _expressionStr[i] !== ")") {
+                    operationsCount++;
+                    operandCount = 0;
+                }
+            }
+            if (_expressionStr[i] === "(") {
+                openBracket = true;
+            } else if (_expressionStr[i] === ")") {
+                if (!openBracket) {
+                    throw "Exception: invalid brackets direction";
+                } else {
+                    closeBracket = true;
+                    openBracket = false;
+                }
+            }
+            if (isKyr || invSym) {
+                throw "Exception: invalid symbol(-s)"
+            }
+
             if (!(operations.has(_expressionStr[i]))) {
                 resultStack.push(_expressionStr[i]);
             } else {
-                let lastIndex = resultStack.length - 1;
-
-                let current = resultStack[lastIndex];
-                let previous = resultStack[lastIndex - 1];
-
-                resultStack.pop();
-                resultStack.pop();
-
-                resultStack.push(previous);
-                resultStack.push(_expressionStr[i]);
-                resultStack.push(current);
+                if (resultStack.length >= 3) {
+                    let last = resultStack.pop();
+                    let prev = resultStack.pop();
+    
+                    let temp ="(" + prev + _expressionStr[i] + last + ")";
+                    resultStack.push(temp);
+                    //resultStack.unshift("(");
+                } else {
+                    operationStack.push(_expressionStr[i]);
+                }
+                
             }
         }
     }
-}
+
  
+if (bracketCount % 2 != 0) {
+    throw "Exception: bracket pass";
+}
 
-
+if (operandCount > operationsCount + 1) {
+    throw "Exception: operation pass";
+}
 
 if (operationStack.length != 0) {
     for (let i = 0; i <= operationStack.length; i++) {
@@ -134,7 +190,9 @@ for (let i = 0; i < resultStack.length; i++) {
     if (resultStack[i] != ")" && resultStack[i] != "(") {
         answer += resultStack[i];
     }
-    
+    else if (!toRPN) {
+        answer += resultStack[i];
+    }
 }
 
 console.log("main.js" + " " + _expressionStr + " " + "-->" + " " + answer);
